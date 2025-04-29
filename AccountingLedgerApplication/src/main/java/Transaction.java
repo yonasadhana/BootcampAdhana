85b24d1a14561dea06014fedf7c2989816d103a3
yonina7do <yonimiracle@gmail.com>
2025-04-29 12:57:36 -0400

    updated transaction class

--- AccountingLedgerApplication/src/main/java/Transaction.java
+++ AccountingLedgerApplication/src/main/java/Transaction.java
@@ -1,3 +1,5 @@
+package com.accountingledger;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -5,44 +7,42 @@ public class Transaction {
     private LocalDate date;
     private LocalTime time;
     private String description;
+    private String vendor;
     private double amount;
 
-    public Transaction(LocalDate date, LocalTime time, String description, double amount) {
+    public Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount) {
         this.date = date;
         this.time = time;
         this.description = description;
+        this.vendor = vendor;
         this.amount = amount;
     }
 
+
     public LocalDate getDate() {
         return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
     }
 
     public LocalTime getTime() {
         return time;
     }
 
-    public void setTime(LocalTime time) {
-        this.time = time;
-    }
-
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public String getVendor() {
+        return vendor;
     }
 
     public double getAmount() {
         return amount;
     }
 
-    public void setAmount(double amount) {
-        this.amount = amount;
+    @Override
+    public String toString() {
+        return String.format("%s|%s|%s|%s|%.2f", date.toString(),
+                time.toString(),
+                description, vendor, amount);
     }
 }
