80685d1a35bfcbe48510e0b1307b7627ff425802
yonina7do <yonimiracle@gmail.com>
2025-04-30 15:41:49 -0400

    started working on the ledger class

--- AccountingLedgerApplication/src/main/java/com/accountingledger/Ledger.java
+++ AccountingLedgerApplication/src/main/java/com/accountingledger/Ledger.java
@@ -1,4 +1,51 @@
 package com.accountingledger;
 
+import java.util.ArrayList;
+
 public class Ledger {
+    // Store all transactions
+    private ArrayList<Transaction> transactions;
+
+    // Constructor to load transactions
+    public Ledger() {
+        // Load transactions from file when Ledger is created
+        this.transactions = FileManager.loadTransactions();
+    }
+
+    // to get all transactions
+    public ArrayList<Transaction> getAllTransactions() {
+        return transactions;
+    }
+
+    // to get only deposit transactions
+    public ArrayList<Transaction> getDeposits() {
+        ArrayList<Transaction> deposits = new ArrayList<>();
+
+        for (Transaction transaction : transactions) {
+            if (transaction.getAmount() > 0) {
+                deposits.add(transaction);
+            }
+        }
+
+        return deposits;
+    }
+
+    // to geet only payment transactions
+    public ArrayList<Transaction> getPayments() {
+        ArrayList<Transaction> payments = new ArrayList<>();
+
+        for (Transaction transaction : transactions) {
+            if (transaction.getAmount() < 0) {
+                payments.add(transaction);
+            }
+        }
+
+        return payments;
+    }
+
+    //to add new transaction
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+        FileManager.saveTransaction(transaction);
+    }
 }
